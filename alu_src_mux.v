60b176705501c03ab294bcfa2a6c6020f0898030
hamnaaqamar <hamnaqamar2004@gmail.com>
2026-04-23 19:07:28 +0500

    writeback mux and alu src mux added

---
+++ alu_src_mux.v
@@ -0,0 +1,11 @@
+module alu_src_mux(
+    input [31:0] read_data2,
+    input [31:0] imm,
+    input ALUSrc,
+    output reg [31:0] alu_src
+);
+
+always @(*) begin
+    alu_src = (ALUSrc) ? imm : read_data2;
+end
+endmodule
---
+++ writeback_mux.v
@@ -0,0 +1,11 @@
+module writeback_mux(
+    input [31:0] alu_result,
+    input [31:0] read_data,
+    input MemtoReg,
+    output reg [31:0] write_data
+);
+
+always @(*) begin
+    write_data = (MemtoReg) ? read_data : alu_result;
+end
+endmodule
