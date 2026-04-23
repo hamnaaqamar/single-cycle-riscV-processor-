a0596bf3530043c3a770e264df92380fe923e835
hamnaaqamar <hamnaqamar2004@gmail.com>
2026-04-23 19:05:32 +0500

    pc mux added

---
+++ pc_mux.v
@@ -0,0 +1,11 @@
+module pc_mux(
+    input [31:0] pc_plus_4,
+    input [31:0] branch_target,
+    input pc_src,
+    output reg [31:0] pc_next
+);
+
+always @(*) begin
+    pc_next = (pc_src) ? branch_target : pc_plus_4;
+end
+endmodule
